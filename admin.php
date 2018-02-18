52c431ad8e7548f464262ecf9588893129785936
Rupert <rupertdenoga2697@gmail.com>
2018-02-18 12:23:14 +0800

    names

--- admin.php
+++ admin.php
@@ -23,14 +23,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a> 
+          <a class="navbar-brand" href="#">Laboy Baboy Tours</a> 
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
             
             <li><a href="#"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
-
+            <li><a href="login.php"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
           </ul>
         </div>
       </div>
@@ -40,23 +39,23 @@
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-            <li><a href="admin.php?page=reports">Reports</a></li>
-            <li><a href="admin.php?page=analytics">Analytics</a></li>
-            <li><a href="admin.php?page=export">Export</a></li>
+            <li class="active"><a href="#">Bookings <span class="sr-only">(current)</span></a></li>
+            <li><a href="admin.php?page=reports">Lookup Category</a></li>
+            <li><a href="admin.php?page=analytics">Lookup Item</a></li>
+            <li><a href="admin.php?page=export">Guests</a></li>
           </ul>
           <ul class="nav nav-sidebar">
-            <li><a href="">Nav item</a></li>
+            <li><a href="">Tours</a></li>
+            <li><a href="">Coordinators</a></li>
+            <li><a href="">Van Contacts</a></li>
+            <li><a href="">Reports</a></li>
+            <li><a href="">Audit Trail</a></li>
+          </ul>
+          <!-- <ul class="nav nav-sidebar">
             <li><a href="">Nav item again</a></li>
             <li><a href="">One more nav</a></li>
             <li><a href="">Another nav item</a></li>
-            <li><a href="">More navigation</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-          </ul>
+          </ul> -->
         </div>
 
         <div class="container-fluid laboy-content">
